b1b94dc48393d9540c0b6dfa56c3d1f40ac52cef
azhansyakirin <azhansyakirin@gmail.com>
2024-03-06 23:29:15 +0800

    seperate components

--- src/components/Popover/index.jsx
+++ src/components/Popover/index.jsx
@@ -1,21 +1,24 @@
 import React from 'react';
 
-export const Popover = ({list, onSelectCategory}) => {
+export const Popover = ({ list, onSelectCategory, closePopover }) => {
 
-    if(!list) return null;
+  if (!list) return null;
 
-    return (
-        <div className="relative text-black bg-white p-8 rounded shadow-md text-center">
-          <ul>
-            {list.map((cat, index) => (
-              <li
-                key={index}
-                className="cursor-pointer p-1 hover:text-blue-400"
-                onClick={()=>onSelectCategory(cat)}>
-                {cat}
-              </li>
-            ))}
-          </ul>
-        </div>
-    )
+  return (
+    <div className="relative text-white bg-neutral-700 p-8 rounded shadow-md text-center">
+      <ul>
+        {list.map((cat, index) => (
+          <li
+            key={index}
+            className="cursor-pointer p-1 hover:text-[#FFC800]"
+            onClick={() => { 
+              onSelectCategory(cat); 
+              closePopover(); 
+              }}>
+            {cat}
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
 }
