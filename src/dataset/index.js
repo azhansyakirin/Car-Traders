063492332dbb6a648e7fe334da53ad4220a7dba5
azhansyakirin <azhansyakirin@gmail.com>
2024-03-06 23:28:56 +0800

    new dataset

--- src/dataset/index.js
+++ src/dataset/index.js
@@ -1,11 +1,14 @@
 const car = [
-    { type: "electric", variant: "executive", category: "sedan", name: "Tesva X", engineCapacity: "1.5", price: 150000, img: "/static/img/tesva.jpg" },
-    { type: "sport", variant: "advance", category: "sedan", name: "Lampoghini", engineCapacity: "5.0", price: 1500000, img: "/static/img/lambo.jpg" },
-    { type: "trekking", variant: "basic", category: "suv", name: "Beep", engineCapacity: "3.0", price: 470000, img: "/static/img/jeep.jpg" },
-    { type: "city", variant: "executive", category: "compact", name: "Axea", engineCapacity: "1.0", price: 22000, img: "/static/img/axia.jpg" },
-    { type: "city", variant: "basic", category: "sedan", name: "Siti", engineCapacity: "1.5", price: 89000, img: "/static/img/city.jpg" },
-    { type: "city", variant: "basic", category: "compact", name: "Myvey", engineCapacity: "1.0", price: 55000, img: "/static/img/myvi.jpg" },
-    { type: "city", variant: "advance", category: "mpv", name: "Westima", engineCapacity: "2.0", price: 110000, img: "/static/img/westima.jpg" },
+    { type: "electric", variant: "executive", category: "sedan", name: "Tesva G", engineCapacity: "0.0", price: 150000, img: "/static/img/tesva.jpg" },
+    { type: "electric", variant: "advance", category: "sedan", name: "Pyd Seel", engineCapacity: "0.0", price: 145000, img: "/static/img/pyd.jpg" },
+    { type: "sport", variant: "advance", category: "sedan", name: "Lampoghini Galapagos", engineCapacity: "5.0", price: 1500000, img: "/static/img/lambo.jpg" },
+    { type: "trekking", variant: "basic", category: "suv", name: "Beep Wiggler", engineCapacity: "3.0", price: 470000, img: "/static/img/jeep.jpg" },
+    { type: "trekking", variant: "advance", category: "suv", name: "Photon Y50", engineCapacity: "2.0", price: 130000, img: "/static/img/y50.jpg" },
+    { type: "city", variant: "executive", category: "compact", name: "Perotiga Axea", engineCapacity: "1.0", price: 22000, img: "/static/img/axia.jpg" },
+    { type: "city", variant: "basic", category: "sedan", name: "Wonda Siti", engineCapacity: "1.5", price: 89000, img: "/static/img/city.jpg" },
+    { type: "city", variant: "basic", category: "compact", name: "Perotiga Myvey", engineCapacity: "1.0", price: 55000, img: "/static/img/myvi.jpg" },
+    { type: "city", variant: "advance", category: "mpv", name: "Totoya Westima", engineCapacity: "2.0", price: 110000, img: "/static/img/westima.jpg" },
+    { type: "city", variant: "advance", category: "mpv", name: "Totoya Welfarer", engineCapacity: "3.0", price: 200000, img: "/static/img/welfarer.jpg" },
 ]
 
 export { car }
