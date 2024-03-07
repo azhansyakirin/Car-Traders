6d02eadb2231880189e3b91bd4cb21966ec89e6b
azhansyakirin <azhansyakirin@gmail.com>
2024-03-07 14:20:54 +0800

    changes on config

--- src/dataset/index.js
+++ src/dataset/index.js
@@ -1,16 +1,16 @@
 const car = [
-    { recommended: true, type: "electric", variant: "executive", category: "sedan", name: "Tesva G", engineCapacity: "0.0", price: 150000, img: "/static/img/tesva.jpg" },
-    { recommended: false, type: "electric", variant: "advance", category: "sedan", name: "Pyd Seel", engineCapacity: "0.0", price: 145000, img: "/static/img/pyd.jpg" },
-    { recommended: false, type: "racing", variant: "advance", category: "sport", name: "Lampoghini Galapagos", engineCapacity: "5.0", price: 1500000, img: "/static/img/lambo.jpg" },
-    { recommended: true, type: "racing", variant: "advance", category: "sport", name: "Ferroro LA", engineCapacity: "6.0", price: 1500000, img: "/static/img/ferroro.jpg" },
-    { recommended: false, type: "trekking", variant: "basic", category: "suv", name: "Beep Wiggler", engineCapacity: "3.0", price: 470000, img: "/static/img/jeep.jpg" },
-    { recommended: true, type: "trekking", variant: "advance", category: "suv", name: "Rage Roger Protector", engineCapacity: "4.0", price: 800000, img: "/static/img/defender.jpg" },
-    { recommended: true, type: "trekking", variant: "advance", category: "suv", name: "Photon Y50", engineCapacity: "2.0", price: 130000, img: "/static/img/y50.jpg" },
-    { recommended: true, type: "city", variant: "executive", category: "compact", name: "Perotiga Axea", engineCapacity: "1.0", price: 22000, img: "/static/img/axia.jpg" },
-    { recommended: false, type: "city", variant: "basic", category: "sedan", name: "Wonda Siti", engineCapacity: "1.5", price: 89000, img: "/static/img/city.jpg" },
-    { recommended: false, type: "city", variant: "basic", category: "compact", name: "Perotiga Myvey", engineCapacity: "1.0", price: 55000, img: "/static/img/myvi.jpg" },
-    { recommended: false, type: "city", variant: "advance", category: "mpv", name: "Totoya Westima", engineCapacity: "2.0", price: 110000, img: "/static/img/westima.jpg" },
-    { recommended: true, type: "city", variant: "advance", category: "mpv", name: "Totoya Welfarer", engineCapacity: "3.0", price: 200000, img: "/static/img/welfarer.jpg" },
+    { recommended: true, type: "electric", variant: "executive", category: "sedan", name: "Tesva G", engineCapacity: "0.0", price: 150000, img: "/car-traders/static/img/tesva.jpg" },
+    { recommended: false, type: "electric", variant: "advance", category: "sedan", name: "Pyd Seel", engineCapacity: "0.0", price: 145000, img: "/car-traders/static/img/pyd.jpg" },
+    { recommended: false, type: "racing", variant: "advance", category: "sport", name: "Lampoghini Galapagos", engineCapacity: "5.0", price: 1500000, img: "/car-traders/static/img/lambo.jpg" },
+    { recommended: true, type: "racing", variant: "advance", category: "sport", name: "Ferroro LA", engineCapacity: "6.0", price: 1500000, img: "/car-traders/static/img/ferroro.jpg" },
+    { recommended: false, type: "trekking", variant: "basic", category: "suv", name: "Beep Wiggler", engineCapacity: "3.0", price: 470000, img: "/car-traders/static/img/jeep.jpg" },
+    { recommended: true, type: "trekking", variant: "advance", category: "suv", name: "Rage Roger Protector", engineCapacity: "4.0", price: 800000, img: "/car-traders/static/img/defender.jpg" },
+    { recommended: true, type: "trekking", variant: "advance", category: "suv", name: "Photon Y50", engineCapacity: "2.0", price: 130000, img: "/car-traders/static/img/y50.jpg" },
+    { recommended: true, type: "city", variant: "executive", category: "compact", name: "Perotiga Axea", engineCapacity: "1.0", price: 22000, img: "/car-traders/static/img/axia.jpg" },
+    { recommended: false, type: "city", variant: "basic", category: "sedan", name: "Wonda Siti", engineCapacity: "1.5", price: 89000, img: "/car-traders/static/img/city.jpg" },
+    { recommended: false, type: "city", variant: "basic", category: "compact", name: "Perotiga Myvey", engineCapacity: "1.0", price: 55000, img: "/car-traders/static/img/myvi.jpg" },
+    { recommended: false, type: "city", variant: "advance", category: "mpv", name: "Totoya Westima", engineCapacity: "2.0", price: 110000, img: "/car-traders/static/img/westima.jpg" },
+    { recommended: true, type: "city", variant: "advance", category: "mpv", name: "Totoya Welfarer", engineCapacity: "3.0", price: 200000, img: "/car-traders/static/img/welfarer.jpg" },
 ]
 
 export { car }
